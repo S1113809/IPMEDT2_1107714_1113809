e242402a78eebdaf5199c9c7322a1d3ea7767b70
Cooldogs111 <59568739+S1113809@users.noreply.github.com>
2021-10-26 22:06:55 +0200

    Removed not working animation

--- JS/game.js
+++ JS/game.js
@@ -15,13 +15,10 @@ function stars(numberOfStars, divID){
         bigRange = Array.apply(null, Array(100)).map(function (_, i) {return i;});
         smallRange = Array.apply(null, Array(3)).map(function (_, i) {return i;});
         tenRange = Array.apply(null, Array(5)).map(function (_, i) {return i;});
-        starTwinkleStage = randomFrom("9","13");
         const top = randomFrom(bigRange);
         const right = randomFrom(bigRange);
         const width = randomFrom(smallRange);
-        text += "<style></style>";
         text += "<div class='stars' style='top:" + top + "%; right: "+ right +"%; width:" + width + "px; height:" + width + "px;";
-        text += "animation: twinkling " + starTwinkleStage + "s infinite";
         text += ";box-shadow: 0px 0px 1vw rgba(255, 255, 255, 0.2);'></div>";
         chosenDiv.innerHTML = text;
         chosenDiv.style.display = "block";
