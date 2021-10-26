1ba9f2a073e9457b764c86f8d34a1d30615522f9
Cooldogs111 <59568739+S1113809@users.noreply.github.com>
2021-10-26 21:56:26 +0200

    Added star background

---
+++ JS/game.js
@@ -0,0 +1,31 @@
+console.log("JS LOADED");
+
+function stars(numberOfStars, divID){
+    const chosenDiv = document.getElementById(divID)
+    chosenDiv.style.display = "none";
+    chosenDiv.innerHTML = "";
+
+    function randomFrom(array) {
+      return array[Math.floor(Math.random() * array.length)];
+    }
+
+    let text = "";
+    let i;
+    for (i = 0; i < numberOfStars; i++) {
+        bigRange = Array.apply(null, Array(100)).map(function (_, i) {return i;});
+        smallRange = Array.apply(null, Array(3)).map(function (_, i) {return i;});
+        tenRange = Array.apply(null, Array(5)).map(function (_, i) {return i;});
+        starTwinkleStage = randomFrom("9","13");
+        const top = randomFrom(bigRange);
+        const right = randomFrom(bigRange);
+        const width = randomFrom(smallRange);
+        text += "<style></style>";
+        text += "<div class='stars' style='top:" + top + "%; right: "+ right +"%; width:" + width + "px; height:" + width + "px;";
+        text += "animation: twinkling " + starTwinkleStage + "s infinite";
+        text += ";box-shadow: 0px 0px 1vw rgba(255, 255, 255, 0.2);'></div>";
+        chosenDiv.innerHTML = text;
+        chosenDiv.style.display = "block";
+    }
+}
+
+stars(80, "js--background")
