fa839fc6760656be672af2718a5ec8c7ff4b94a7
Kevin Lammersen <kevin77@hotmail.nl>
2021-10-27 12:22:11 +0200

    changed variable

--- JS/background.js
+++ JS/background.js
@@ -5,29 +5,29 @@ const buttonLeft = document.getElementById("js--buttonLeft");
 const background1 = document.getElementById("js--background1");
 const background2 = document.getElementById("js--background2");
 
-let stage = 1;
+let stage = 0;
 
 buttonRight.addEventListener("click", function(){
-    setStageForward(stage);
     stage++;
+    setStageForward(stage);
     checkStage();
     console.log(stage);
   }
 );
 
 buttonLeft.addEventListener("click", function(){
-    stage--;
     setStageBack(stage);
+    stage--;
     checkStage();
     console.log(stage);
 });
 
 function checkStage(){
   switch (stage){
-    case 1:
+    case 0:
       buttonLeft.classList.add("navButton--noClick");
       break;
-    case 2:
+    case 1:
       buttonLeft.classList.remove("navButton--noClick");
       break;
   }
@@ -35,12 +35,14 @@ function checkStage(){
 
 function setStageForward(stage){
   const translation = stage * 100;
+  const prevTranslation = translation - 100;
   background1.style.transform= "translateX(" + translation + "vw)";
   background2.style.transform= "translateX(-" + translation + "vw)";
+
 }
 
 function setStageBack(stage){
-  if (stage == 1) {
+  if (stage == 0) {
     background1.style.transform= "none";
     background2.style.transform= "none";
   }else {
