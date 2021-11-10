95dddfdaa57461c2532b3e46e80fe5d440ee3a80
Cooldogs111 <59568739+S1113809@users.noreply.github.com>
2021-11-10 22:33:10 +0100

    Final pengu fixes with clickablility

--- JS/penguin.js
+++ JS/penguin.js
@@ -1,9 +1,10 @@
+// Validator: no errors
 const textBubble = document.getElementById("js--pengu-textBubble");
 window.onload = function ()  {
     togglePenguin();
 }
 // Toggle hidden or show on textbubble
-function togglePenguin(){    
+function togglePenguin(){
     textBubble.toggleAttribute('hidden');
 }
 // Close bubble on click button or penguin
