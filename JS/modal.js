949054d40888706e9e7b3b20091689eb4eb6d213
Cooldogs111 <59568739+S1113809@users.noreply.github.com>
2021-11-10 19:50:35 +0100

    Added jupiter

--- JS/modal.js
+++ JS/modal.js
@@ -7,6 +7,10 @@ const modal2 = document.getElementById("js--modal2");
 const planet2 = document.getElementById("js--planet2");
 const span2 = document.getElementById("js--modalClose2");
 
+const modal3 = document.getElementById("js--modal3");
+const planet3 = document.getElementById("js--planet3");
+const span3 = document.getElementById("js--modalClose3");
+
 //funties modal 1
 planet1.onclick = function() {
   modal1.style.display = "block";
@@ -34,5 +38,20 @@ span2.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal2) {
     modal2.style.display = "none";
+  }
+}
+
+//funties modal 3
+planet3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
   }
 }
